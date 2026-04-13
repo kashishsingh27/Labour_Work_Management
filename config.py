087a1bb600fad3659a1a5278b929314b15f772bb
Khushi-Devi <devikhushi466@gmail.com>
2026-04-13 15:37:52 +0530

    remove secrets from config, use environment variables

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-dev-key")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///site.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-
-    MAIL_USERNAME = "devikhushi466@gmail.com"
-    MAIL_PASSWORD = "iufbtipklevgzdok"
-
-    MAIL_DEFAULT_SENDER = "devikhushi466@gmail.com"
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
