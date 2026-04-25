1666894fb9c9f724e65a828a086818370be1f984
Khushi-Devi <devikhushi466@gmail.com>
2026-04-25 23:58:02 +0530

    cleaned nodels.py for better readablity

--- app/models.py
+++ app/models.py
@@ -5,16 +5,6 @@ from datetime import datetime
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-#class User(db.Model, UserMixin):
-#    id = db.Column(db.Integer, primary_key=True)
-#    username = db.Column(db.String(120), nullable=False)
-#    email = db.Column(db.String(120), unique=True, nullable=False)
-#    password = db.Column(db.String(200), nullable=False)
-#    role = db.Column(db.String(20), nullable=False)  # contractor / labour#
-#
-#    def __repr__(self):
-#        return f"User('{self.username}', '{self.role}')"*/
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
 
