8a499ddd9989f6b509fda31b59e1e8a8571f9720
thesurgicalcoder <mradul.khandelwal_cs.aiml24@gla.ac.in>
2026-05-01 05:04:51 +0530

    applied logging to tha application for visiblity

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from config import Config
-from app.extensions import db, login_manager, bcrypt
-from app.extensions import mail
-from app.extensions import migrate
+from app.extensions import db, login_manager, bcrypt, mail, migrate
+import logging
 
 
 def create_app():
     """Application factory — creates and configures the Flask app instance."""
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Setup logging INSIDE app context
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        handlers=[
+            #logging.FileHandler("app.log"),  
+            logging.StreamHandler()  
+        ]
+    )
 
     # Initialise extensions with the app instance
     db.init_app(app)
@@ -20,7 +29,7 @@ def create_app():
     # Redirect unauthenticated users to the login page
     login_manager.login_view = "auth.login"
 
-    # Register blueprints — imports are inside the function to avoid circular imports
+    # Register blueprints — imports inside function to avoid circular imports
     from app.auth.routes import auth
     from app.contractor.routes import contractor
     from app.labour.routes import labour
