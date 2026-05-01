67bfd038cfd032405e7ad72b3fc3e77484bc072b
thesurgicalcoder <mradul.khandelwal_cs.aiml24@gla.ac.in>
2026-05-01 07:42:53 +0530

    fix: add MAIL_TIMEOUT=10 to prevent gunicorn worker SIGKILL on SMTP hang

--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
+    MAIL_TIMEOUT = 10 
